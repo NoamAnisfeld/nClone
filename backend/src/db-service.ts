994dedb94cf805431aecd787913c6a1cdfa82dfb
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-05-27 17:48:27 +0300

    pseudo DB

---
+++ backend/src/db-service.ts
@@ -0,0 +1,47 @@
+import type { DB, Link, LinkCreationInfo } from "./schemas.js";
+import { dbSchema } from "./schemas.js";
+import { generateId } from "./utils.js";
+import fs from 'node:fs';
+
+const DB_FILE_PATH = 'pseudo-db.json';
+const _db = _retriveDb();
+
+function _retriveDb(): DB {
+    try {
+        const fileContents = fs.readFileSync(DB_FILE_PATH);
+        const json = JSON.parse(fileContents.toString());
+        const validated = dbSchema.parse(json);
+        return validated;
+    } catch (e) {
+        console.error('Error: Unable to read from the pseudo DB file or data is malformed\n', e);
+        return {
+            links: [],
+        };
+    }
+}
+
+function _persistDb() {
+    fs.writeFile(
+        DB_FILE_PATH,
+        JSON.stringify(_db, undefined, 4),
+        error => error && console.log('Error saving DB data to file:\n', error),
+    );    
+}
+
+export async function addLink(creationInfo: LinkCreationInfo) {
+
+    const newLink: Link = {
+        id: generateId(),
+        title: creationInfo.title,
+        author: creationInfo.author,
+        commentIds: [],
+        votesCount: 0,
+    };
+    _db.links.push(newLink);
+    _persistDb();
+    return newLink;
+}
+
+export async function getLinks() {
+    return _db.links;
+}
