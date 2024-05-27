bb4b2340fbfbff6e92c556dae6aefa07091ea630
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-05-27 17:46:16 +0300

    data schemas

---
+++ backend/src/schemas.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const linkCreationInfoSchema = z.object({
+    title: z.string(),
+    author: z.string(),
+});
+export type LinkCreationInfo = z.infer<typeof linkCreationInfoSchema>;
+
+export const linkSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    author: z.string(),
+    commentIds: z.array(z.string()),
+    votesCount: z.number(),
+});
+export type Link = z.infer<typeof linkSchema>;
+
+export const dbSchema = z.object({
+    links: z.array(linkSchema),
+});
+export type DB = z.infer<typeof dbSchema>;
